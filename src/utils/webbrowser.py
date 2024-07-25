c01d86bdfec0e44805cc02301b0dd455f4bfdaf8
JackSSads <fotosjackson2018@hotmail.com>
2024-07-25 13:04:41 -0300

    Adicionando comentários

--- src/utils/webbrowser.py
+++ src/utils/webbrowser.py
@@ -7,6 +7,7 @@ class WebBrowser():
         self.url = url
         self.browser = None
 
+    # Inicializa um webdriver, acessa a url enviada na instância do objeto
     def initalise_web_browser(self):
         try:
             print("Iniciando web browser")
@@ -17,8 +18,10 @@ class WebBrowser():
         except Exception as e:
             print(f"Ocorreu um erro no initalise_web_browser(): {e}")
 
+    # esportando a conexão
     def atribuites(self) -> webdriver:
         return self.browser
     
+    # Método de espera por elemento
     def driver_wait(self, driver: webdriver, element: any, time: int = 10) -> WebDriverWait:
          return WebDriverWait(driver, time).until(EC.element_to_be_clickable(element))
