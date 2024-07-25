814f9a2432b7bda4ecef0aa7ca6dc0f05d625334
JackSSads <fotosjackson2018@hotmail.com>
2024-07-25 03:15:55 -0300

    removendo desnecessário

--- src/resources/linkedin_jobs.py
+++ src/resources/linkedin_jobs.py
@@ -1,4 +1,3 @@
-from selenium import webdriver
 from flask import Blueprint, request, jsonify
 from src.service.linkedin_service import LinkedinService
 from src.service.whatsapp_service import WhatsappService
@@ -8,8 +7,6 @@ robo_resource = Blueprint("robo", __name__, url_prefix="/robo")
 @robo_resource.route("/", methods=["GET"])
 def get_jobs():
     try:
-        browser = webdriver.Chrome()
-
         req = request.json
         robo_linkedin = LinkedinService(username=req['username'], password=req["password"], search=req["search"])
         robo_linkedin.init_robo_linkedin()
