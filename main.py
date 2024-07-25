5fcac4117cb7611b14c458ad29cd4b5dc0c64d5e
JackSSads <fotosjackson2018@hotmail.com>
2024-07-25 13:01:22 -0300

    Removendo verificação

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ class Main:
         self.robo_linkedin = RoboLinkedin()
         self.robo_whatsapp = RoboWhatsapp()
 
-    def start_robo_linkedin(self, username: str, passowrd: str, search: str, validation: int):
+    def start_robo_linkedin(self, username: str, passowrd: str, search: str):
         try:
-            return self.robo_linkedin.initialise_robo(username, passowrd, search, validation)
+            return "self.robo_linkedin.initialise_robo(username, passowrd, search)"
         except Exception as e:
-            print(f"Ocorreu um erro no start_robo_linkedin(): {e}")
+            return print(f"Ocorreu um erro no start_robo_linkedin(): {e}")
     
     def start_robo_whatsapp(self, phone: int):
         try:
             return self.robo_whatsapp.initialise_robo_whatsapp(phone)
         except Exception as e:
-            print(f"Ocorreu um erro no start_robo_whatsapp(): {e}")
+            return print(f"Ocorreu um erro no start_robo_whatsapp(): {e}")
