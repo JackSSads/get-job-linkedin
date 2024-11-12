072b04c9c2e5ac22dc30e0f6687cb1364df9427a
JackSSads <contatojacksonsouza@hotmail.com>
2024-11-12 14:22:44 -0300

    atualizando automação com Selenium

--- robos/robo_linkedin.py
+++ robos/robo_linkedin.py
@@ -1,87 +1,81 @@
-from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+
 import pandas as pd
 from time import sleep
 
-from src.utils import WebBrowser
+from src.utils import Driver
+
+driver = Driver()
 
 class RoboLinkedin:
 
     def __init__(self) -> None:
-        self.browser = WebBrowser(url="https://www.linkedin.com/login")
+        pass
 
     def validation(self):
-        control = True
-        while control:
+        # Caso haja um pop-up de validação de email, aguarda até dois minutos
+        control = 0
+        while control < 120:
             try:
-                self.browser.driver_wait(
-                    driver=self.browser.atribuites(),
-                    element=(By.ID, 'input__email_verification_pin'),
-                    time=10
-                )
+                inputVerification = driver.waiting("//input[@id='input__email_verification_pin']")
 
-                print("Aguardando validação")
+                if inputVerification:
+                    print("Aguardando validação")
 
-                sleep(2)
-            except Exception as e:
-                control = False
-                return print("Linkedin validado!")
+                    sleep(2)
+                    control += 2
+                else: 
+                    raise Exception("Não encontrou validação")
+            except Exception:
+                control = 120
+                print("Não encontrou validação")
+                return
 
     def get_job(self, search: str):
         try:
-            # button search
-            self.browser.driver_wait(
-                driver=self.browser.atribuites(), 
-                element=(By.CLASS_NAME, 'search-global-typeahead__collapsed-search-button'), 
-                time=10
-            ).click()
             
             # input search
-            input_search_job = self.browser.driver_wait(
-                driver=self.browser.atribuites(), 
-                element=(By.CLASS_NAME, 'search-global-typeahead__input'), 
-                time=10
-            )
+            print("Buscando botão de busca...")
+            input_search_job = driver.waiting("//div[@id='global-nav-typeahead']/input")
             input_search_job.click()
-            input_search_job.send_keys(search)
-            input_search_job.send_keys(Keys.RETURN)
+            driver.write(input_search_job, search)
+            driver.keys(input_search_job, Keys.RETURN)
             
             print("Buscando vagas...")
 
             # Clicar no filtro "Vagas"
-            self.browser.driver_wait(
-                driver=self.browser.atribuites(),
-                element=(By.XPATH, '//*[@id="search-reusables__filters-bar"]/ul/li[1]'),
-                time=15
-            ).click()
+            driver.waiting("//div[@id='search-reusables__filters-bar']//button[text()='Vagas']").click()
 
             sleep(5)
 
             # buscando elemento footer para fazer o scroll
-            scroll = self.browser.driver_wait(
-                driver=self.browser.atribuites(),
-                element=(By.CLASS_NAME, 'global-footer-compact'),
-                time=15
-            )
+            scroll = driver.waiting("//footer")
+
+            # TODO Corrigir esse scroll
             if scroll:
                 # Execute o script para rolar até o elemento
-                self.browser.atribuites().execute_script("arguments[0].scrollIntoView(true);", scroll)
-                print("Scrollado para o elemento Footer.")
-                sleep(5)
-
+                driver.scroll(scroll)
+                sleep(3)
             else:
                 print("Elemento Footer não encontrado, scrolling falhou.")
 
+            # TODO verificar se os dados que estão sendo capturados estão corretos
             # Capturando texto e link da vaga
-            list_elements = self.browser.atribuites().find_elements(By.CLASS_NAME, 'job-card-container__link')
+            list_elements = driver.waiting(
+                ec=EC.visibility_of_all_elements_located,
+                search="//ul[@class='scaffold-layout__list-container']/li//div/div/div/div/div[2]/div/a"
+            )
+
             text_job = [job.text for job in list_elements]
             link_job = [job.get_attribute("href") for job in list_elements]
                        
             self.insert_data_in_excel_file(text_job, link_job)
 
             print("Terminado")
-            return self.browser.atribuites().quit()
+            return driver.close_web_driver()
         except Exception as e:
+            driver.close_web_driver()
             return print(f"Ocorreu um erro no get_job(): {e}")
 
     def insert_data_in_excel_file(self, jobs, links):
@@ -101,27 +95,17 @@ class RoboLinkedin:
     def initialise_robo(self, username: str, passowrd: str, search: str):
         
         print("Iniciando robô linkedin")
-        self.browser.initalise_web_browser()
+        driver.initialize_webdriver()
+
+        driver.navigate_to("https://www.linkedin.com/login")
         
-        input_email = self.browser.driver_wait(
-            driver=self.browser.atribuites(),
-            element=(By.ID, 'username'),
-            time=10
-        )
-        input_email.send_keys(username)
+        input_email = driver.waiting("//input[@id='username']")
+        driver.write(input_email, username)
 
-        input_password = self.browser.driver_wait(
-            driver=self.browser.atribuites(),
-            element=(By.ID, 'password'),
-            time=10
-        )
-        input_password.send_keys(passowrd)
+        input_password = driver.waiting("//input[@id='password']")
+        driver.write(input_password, passowrd)
 
-        self.browser.driver_wait(
-            driver=self.browser.atribuites(),
-            element=(By.CLASS_NAME, 'login__form_action_container'),
-            time=10
-        ).click()
+        driver.waiting("//button[@class='btn__primary--large from__button--floating']").click()
 
         self.validation()
 
--- robos/robo_whatsapp.py
+++ robos/robo_whatsapp.py
@@ -1,61 +1,85 @@
-from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+
+import pyautogui
+import pyperclip
+
 import openpyxl
-from urllib.parse import quote
 from time import sleep
 
-from src.utils import WebBrowser
+from src.utils import Driver
+
+driver = Driver()
 
 class RoboWhatsapp:
     def __init__(self) -> None:
-        self.browser = WebBrowser(url="https://web.whatsapp.com/")
+        pass
 
-    def initialise_robo_whatsapp(self, phone: int):
+    def initialise_robo_whatsapp(self, contact: str):
         print("\nIniciando robô whatsapp\n")
-        self.browser.initalise_web_browser()
+        driver.initialize_webdriver()
+        driver.navigate_to("https://web.whatsapp.com/")
 
         print("Faça login no Whatsapp")
 
         # Aguardando login no Whatsapp
         control = True
         while control:
-            try:
-                self.browser.driver_wait(
-                    driver=self.browser.atribuites(),
-                    element=(By.CLASS_NAME, "_akau"),
-                    time=10
-                )
+            qrcode = driver.waiting("//canvas")
 
+            if qrcode:
                 print("Aguardando o login no Whatsapp")
                 sleep(5)
-            except:
-                print("\nLogin realizado\n")
+            else:
                 control = False
+                print("\nLogin realizado\n")
+                self.enviar_mensagem(contact=contact)
+
+    def enviar_mensagem(self, contact: int):
+        def escrever(frase):
+            pyperclip.copy(frase)
+            pyautogui.hotkey("ctrl", "v")
+
+        # Clicando no elemento de busca
+        print("Clicando no elemento de busca")
+        driver.waiting(
+            ec=EC.presence_of_element_located,
+            search="//p[@class='selectable-text copyable-text x15bjb6t x1n2onr6']"
+        ).click()
+
+        sleep(3)
+
+
+        escrever(contact)
+        
+        sleep(1)
+
+        pyautogui.hotkey("enter")
+
+        driver.waiting("//div[@class='_ak1r']/div/div/div/p").click()
 
         print("Buscando arquivo jobs.xlsx\n")
         path_file = "./jobs.xlsx"
         workbook = openpyxl.load_workbook(path_file)
         page_jobs = workbook['Sheet1']
 
-        # criando link personalizado para envio da mensagem
-        for line in page_jobs.iter_rows(min_row=2):
-            print(f"Vaga: {line[1].value}")
-            job = line[1].value
-            link = line[2].value
+        # Escrevendo mensagem
+        for collumn in page_jobs.iter_rows(min_row=2):
+            print(f"Vaga: {collumn[1].value}")
+            job = collumn[1].value
+            link = collumn[2].value
 
-            #https://web.whatsapp.com/send?phone=<go_to>&text=<message>
-            message = f'Vaga: {quote(job)}. Link: {link}'
-            link_message = f'https://web.whatsapp.com/send?phone={phone}&text={message}'
-                        
-            self.browser.atribuites().get(link_message)
-
-            self.browser.driver_wait(
-                driver=self.browser.atribuites(),
-                element=(By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[2]/button'),
-                time=10
-            ).click()
+            escrever(
+                f"""
+                Vaga: {job}
+                Link: {link}
+                """
+            )
             
-            print("\nMensagem enviada\n")
+            pyautogui.hotkey("enter")
+            
+        print("\nMensages enviada\n")
 
-            sleep(2)
+        sleep(2)
 
-        self.browser.atribuites().quit()
+        driver.close_web_driver()
