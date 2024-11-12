b49b54c6dc10b9b873d099122d40a9cdabefa758
JackSSads <contatojacksonsouza@hotmail.com>
2024-11-12 14:23:30 -0300

    renomeando parâmetro da classe

--- src/service/whatsapp_service.py
+++ src/service/whatsapp_service.py
@@ -3,12 +3,12 @@ from main import Main
 main = Main()
 
 class WhatsappService():
-    def __init__(self, phone) -> None:
-        self.phone = phone
+    def __init__(self, contact) -> None:
+        self.contact = contact
 
     def init_robo_whatsapp(self):
         try:
-            res = main.start_robo_whatsapp(self.phone)
+            res = main.start_robo_whatsapp(self.contact)
             print("Robô Whatsapp finalizado")
             return res
         except Exception as e:
